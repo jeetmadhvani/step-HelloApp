26b5edb1d38e8ca9d39e4f7e9e14b6ce28478161
Jeet Madhvani <madhavanijeet123@gmail.com>
2026-03-25 13:37:18 +0530

    UC7

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,19 +1,10 @@
 public class HelloApp {
-
     public static void main(String[] args) {
+        String name="World";
 
-        StringBuilder nameBuilder = new StringBuilder();
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
+        if(args.length>0){
+            name=String.join(", ",args);
         }
-
-        String result = nameBuilder.toString();
-
-        if (result.length() > 0) {
-            result = result.substring(0, result.length() - 2);
-        }
-
-        System.out.println("Hello, " + result + "!");
+        System.out.println("Hello, "+name+"!");
     }
 }
